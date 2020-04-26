6f99bb966056b6143a89f1ce3623214ba3ba1d13
Hamza khan <hamzakhancs15@gmail.com>
2020-04-26 22:08:00 +0500

    Made election contract and added test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity 0.5.16;
 
 contract Election{
+    //Model
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store
+    //Fetch
+    mapping(uint => Candidate) public candidates;
 
-    string public candidate;
+    //Store Count
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+    }
 
+    function addCandidate (string memory _name) private{
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
